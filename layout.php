8c19b60d20f3f73653126fac240833f11579fe95
voopsc <voopstudio@gmail.com>
2021-02-02 12:18:18 +0200

    add(files): create template parts for next backend developing -v. 0.2

---
+++ layout.php
@@ -0,0 +1,6 @@
+<?php
+  define('ROOT', dirname(__FILE__));
+
+  include_once(ROOT . '/parts/header.php');
+  
+  include_once(ROOT . '/parts/footer.php');
---
+++ parts/posts.php
@@ -0,0 +1,71 @@
+<main>
+  <section>
+    <div class="wrapper">
+      <div class="full">
+        <h1 class="title-main">Latest news</h1>
+      </div>
+      <ul class="post-list list-three-items list">
+        <li>
+          <div class="box">
+            <div class="img-wrp">
+              <img src="img/poster.jpg" alt="picture">
+            </div>
+            <div class="title-box ">
+              <h2 class="title"><a href="#">Post one</a></h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit
+                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                 occaecat cupidatat non proident, sunt in culpa qui officia
+                 deserunt mollit anim id est laborum.</p>
+              <a href="#" class="link-text">More...</a>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div class="box">
+            <div class="img-wrp">
+              <img src="img/poster.jpg" alt="picture">
+            </div>
+            <div class="title-box ">
+              <h2 class="title"><a href="#">Post one</a></h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit
+                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                 occaecat cupidatat non proident, sunt in culpa qui officia
+                 deserunt mollit anim id est laborum.</p>
+              <a href="#" class="link-text">More...</a>
+            </div>
+          </div>
+        </li>
+
+        <li>
+          <div class="box">
+            <div class="img-wrp">
+              <img src="img/poster.jpg" alt="picture">
+            </div>
+            <div class="title-box ">
+              <h2 class="title"><a href="#">Post one</a></h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit
+                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                 occaecat cupidatat non proident, sunt in culpa qui officia
+                 deserunt mollit anim id est laborum.</p>
+              <a href="#" class="link-text">More...</a>
+            </div>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  </section>
+</main>
